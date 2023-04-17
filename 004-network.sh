40d64be8d7ec7435577a31c759879abb08b83f1c
Tao Chen <tachen@phys.ethz.ch>
2023-04-17 12:53:04 +0200

    UPDATE

--- 004-network.sh
+++ 004-network.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 IP_ADDRESS="10.10.0.3"
-
+ETH="eth1"
+NETMASK="255.255.255.0"
 # Check if the user is root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Please run this script as root."
@@ -9,7 +10,14 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Configure the IP address and netmask for eth1 interface
-ip addr add $IP_ADDRESS/24 dev eth1
+cat >> /etc/network/interfaces << EOF
+
+auto $ETH
+iface $ETH inet static
+address $IP_ADDRESS
+netmask $NETMASK
+EOF
 
 # Enable the eth1 interface
-ip link set eth1 up
+ifdown $ETH
+ifup $ETH
