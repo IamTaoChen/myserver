00aa614067e098d9b88704980f8e3ecf985f8da2
Tao Chen <tachen@phys.ethz.ch>
2023-04-17 12:55:47 +0200

    update

--- init.sh
+++ init.sh
@@ -5,17 +5,18 @@
  # @Author: Tao Chen
  # @Date: 2023-04-15 05:58:41
  # @LastEditors: Tao Chen
- # @LastEditTime: 2023-04-17 12:20:30
+ # @LastEditTime: 2023-04-17 12:55:34
 ### 
 
 ROOT=$(dirname $(readlink -f $0))
 
-chmod +x $ROOT/*.sh
+# chmod +x $ROOT/*.sh
 
 ALL_SHELLS=`ls -l "${ROOT}/" | awk '/^-.*[0-9][0-9][0-9]-[A-Za-z]+\.sh$/ {print $NF}' `
 
 for SHELL_FILE in ${ALL_SHELLS[@]}
 do
     echo ">Execute ${SHELL_FILE}"
-    eval $ROOT/$SHELL_FILE
+    # eval $ROOT/$SHELL_FILE
+    bash $ROOT/$SHELL_FILE
 done
